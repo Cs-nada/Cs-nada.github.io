9f4d063a4537b5c4a6459d7265a832995740e8a0
Nada Almehmadi <ndooh@Ndoos-MacBook-Air.local>
2021-03-13 18:35:48 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -184,68 +184,6 @@
   });
 
   /**
-   * Testimonials slider
-   */
-  // new Swiper('.testimonials-slider', {
-  //   speed: 600,
-  //   loop: true,
-  //   autoplay: {
-  //     delay: 5000,
-  //     disableOnInteraction: false
-  //   },
-  //   slidesPerView: 'auto',
-  //   pagination: {
-  //     el: '.swiper-pagination',
-  //     type: 'bullets',
-  //     clickable: true
-  //   },
-  //   breakpoints: {
-  //     320: {
-  //       slidesPerView: 1,
-  //       spaceBetween: 20
-  //     },
-
-  //     1200: {
-  //       slidesPerView: 3,
-  //       spaceBetween: 20
-  //     }
-  //   }
-  // });
-
-  /**
-   * Clients Slider
-   */
-  // new Swiper('.gallery-slider', {
-  //   speed: 400,
-  //   loop: true,
-  //   centeredSlides: true,
-  //   autoplay: {
-  //     delay: 5000,
-  //     disableOnInteraction: false
-  //   },
-  //   slidesPerView: 'auto',
-  //   pagination: {
-  //     el: '.swiper-pagination',
-  //     type: 'bullets',
-  //     clickable: true
-  //   },
-  //   breakpoints: {
-  //     320: {
-  //       slidesPerView: 1,
-  //       spaceBetween: 20
-  //     },
-  //     640: {
-  //       slidesPerView: 3,
-  //       spaceBetween: 20
-  //     },
-  //     992: {
-  //       slidesPerView: 5,
-  //       spaceBetween: 20
-  //     }
-  //   }
-  // });
-
-  /**
    * Initiate gallery lightbox 
    */
   const galleryLightbox = GLightbox({
